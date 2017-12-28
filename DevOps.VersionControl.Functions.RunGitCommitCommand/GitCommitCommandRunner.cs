753ae3dd1c508081b4b7a2b90488cce312a73e29
Christopher Dorst <chris@dorst.email>
2017-12-27 20:36:18 -0600

    Adding GitCommitCommandRunner

---
+++ DevOps.VersionControl.Functions.RunGitCommitCommand/GitCommitCommandRunner.cs
@@ -0,0 +1,16 @@
+﻿using static DevOps.VersionControl.Functions.RunGitCommand.GitCommandRunner;
+
+namespace DevOps.VersionControl.Functions.RunGitCommitCommand
+{
+    public static class GitCommitCommandRunner
+    {
+        private const string commit = nameof(commit);
+        private const string MessageFlag = "-m";
+
+        public static void Commit(string directory, string message)
+            => Git(directory,
+                command: commit,
+                MessageFlag,
+                message);
+    }
+}
